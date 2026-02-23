f9710d43e9f86598f6df667ed418527b6cee9f2e
guihermegor <guirodrigues.gor@gmail.com>
2026-02-23 12:34:51 -0300

    bugfix: prompts folder, and its files, were not being created in python ddd service project

--- scripts/scaffold/python_ddd_service.sh
+++ scripts/scaffold/python_ddd_service.sh
@@ -158,6 +158,7 @@ copy_common_templates() {
     cp "$COMMON_TEMPLATE_ROOT/.github/workflows/tests.yaml" "$project_path/.github/workflows/tests.yaml"
     cp "$COMMON_TEMPLATE_ROOT/.github/CODEOWNERS" "$project_path/.github/CODEOWNERS"
     cp "$COMMON_TEMPLATE_ROOT/.github/PULL_REQUEST_TEMPLATE.md" "$project_path/.github/PULL_REQUEST_TEMPLATE.md"
+    cp -r "$COMMON_TEMPLATE_ROOT/prompts" "$project_path/prompts"
     
     print_status "success" "Common templates applied"
 }
